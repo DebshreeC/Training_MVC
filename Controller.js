d4da85042fd98d1fc5d28130b1401697cb07fc69
DebshreeC <debshreechakraborty@yahoo.co.in>
2017-02-04 16:28:12 +0530

    Add files via upload

---
+++ Controller.js
@@ -0,0 +1,57 @@
+﻿if (typeof HealthCare === 'undefined')
+    HealthCare = {};
+HealthCare.controller = {
+    httpReq: null,
+    createAjaxObject: function () {
+        /*try {
+            this.httpReq = new XMLHttpRequest();
+        }
+        catch (e) {
+            try {
+                this.httpReq = new ActiveXObject("Msxml2.XMLHTTP3.0");
+                console.log("Browser version 7 or more");
+
+            }
+            catch (e) {
+                console.log("AJAX Object can't created");
+                return false;
+            }
+        }
+
+        this.httpReq.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+        this.httpReq.send(null);
+        this.httpReq.onreadystatechange = CallBackResponce;
+        */
+
+
+        var promise = new Promise(function (resolve,reject,request) {
+            var httpRequest = new XMLHttpRequest();
+            httpRequest.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+            httpRequest.send(null);
+
+            httpRequest.onload=function()
+            {
+                if ((httpRequest.readyState == 4) && (httpRequest.status == 200))
+                {
+                    resolve(httpRequest.response);
+                }
+                else
+                {
+                    reject(Error(httpRequest.statusText));
+                }
+            }
+            httpRequest.onerror = function () {               
+                    reject('Call Rejected');
+            }
+        });
+        return promise;
+    }
+};
+    function CallBackResponce()
+    {
+        if((HealthCare.controller.httpReq.readyState==4)&&(HealthCare.controller.httpReq.status==200))
+        {
+            HealthCare.model=HealthCare.controller.httpReq.responseText;
+            console.log(HealthCare.model);
+        }
+    }
---
+++ View.js
@@ -0,0 +1,58 @@
+﻿if (typeof HealthCare === 'undefined')
+    HealthCare = {};
+
+window.onload = init;
+function init()
+{
+    HealthCare.view.display();
+}
+
+HealthCare.view = {
+    display: function () {
+        var promise = HealthCare.controller.createAjaxObject();
+        promise.then(function(data) {
+            HealthCare.model = data;
+            viewData();
+        },
+        function(error)
+        {
+            console.log(error);
+        }
+
+        );
+        //setTimeout(viewData, 4000);
+    }
+
+}
+
+function viewData()
+{
+    console.log(HealthCare.model);
+    var table = document.createElement('Table');
+    table.setAttribute("border", "2");
+    jobj=JSON.parse(HealthCare.model);
+    for(pos in jobj)
+    {
+        tr = document.createElement('tr');
+        td = document.createElement('td');
+        node = document.createTextNode(jobj[pos].id);
+        td.appendChild(node);
+        tr.appendChild(td);
+        td = document.createElement('td');
+        node = document.createTextNode(jobj[pos].username);
+        td.appendChild(node);
+        tr.appendChild(td);
+        td = document.createElement('td');
+        node = document.createTextNode(jobj[pos].address.city);
+        td.appendChild(node);
+        tr.appendChild(td);
+        td = document.createElement('td');
+        node = document.createTextNode(jobj[pos].address.zipcode);
+        td.appendChild(node);
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
+
+    var ref = document.getElementById('response');
+    ref.appendChild(table);
+}
