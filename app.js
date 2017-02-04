0d95e65ce7dcb12e57ad77c52919e1c01204faf6
DebshreeC <debshreechakraborty@yahoo.co.in>
2017-02-04 18:38:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿var appModule = angular.module('InsModule', []);//Module Created
+
+appModule.controller('InsController', ['$scope',function($scope){
+    //console.log("Controller Invoked");
+    $scope.customer = {
+        Id: 0,
+        Name: '',
+        DOB:new Date()
+    }
+
+    $scope.Save=function()
+    {
+        console.log($scope.customer.Id, '', $scope.customer.Name, '', $scope.customer.DOB);
+
+    }
+
+}]);
