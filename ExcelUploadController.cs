49d2921a51163b2a66a75a0531982a1da28924b0
DebshreeC <debshreechakraborty@yahoo.co.in>
2017-02-07 18:28:47 +0530

    Add files via upload

---
+++ ExcelUploadController.cs
@@ -0,0 +1,68 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace ExcelUpload.Controllers
+{
+    public class ExcelUploadController : Controller
+    {
+        //
+        // GET: /ExcelUpload/
+        public String Index()
+        {
+            return "Upload Code is Ready";
+        }
+
+
+        [HttpPost]
+        public String ExcelFilePost(HttpPostedFileBase fileData)
+        {
+            String filename,type;
+            byte[] fileBytes=null;
+            List<ExcelUpload.Models.Insurance_tbl> InsList = new List<ExcelUpload.Models.Insurance_tbl>();
+            if(Request!=null)
+            {
+                for(int i=0;i<Request.Files.Count;i++)
+                {
+                    fileData = Request.Files["UploadedFile"];
+                    if((fileData!=null)&&(fileData.ContentLength>0)&& (!string.IsNullOrEmpty(fileData.FileName)))
+                    {
+                        filename=fileData.FileName;
+                        type=fileData.ContentType;
+                        fileBytes=new byte[fileData.ContentLength];
+                        var StreamedData=fileData.InputStream.Read(fileBytes,0,fileData.ContentLength);
+                        using (var excelObj = new ExcelPackage(fileData.InputStream))
+                        {
+                            var currentSheet = excelObj.Workbook.Worksheets;
+                            var workSheet = currentSheet.First();
+                            var noOfCol=workSheet.Dimension.End.Column;
+                            var noOfRow = workSheet.Dimension.End.Row;
+                            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+                            for(int r=2 ;r<=noOfRow;r++)
+                            {
+                                var insObj=new ExcelUpload.Models.Insurance_tbl();
+                                insObj.DateOfService=Convert.ToDateTime(workSheet.Cells[r,1].Value);
+                                insObj.InsuranceCode = Convert.ToString(workSheet.Cells[r, 2].Value);
+                                insObj.InsuranceId = Convert.ToInt32(workSheet.Cells[r, 3].Value);
+                                insObj.Name = Convert.ToString(workSheet.Cells[r, 4].Value);
+                                insObj.PhoneNo = Convert.ToString(workSheet.Cells[r, 5].Value);
+                                insObj.Address = Convert.ToString(workSheet.Cells[r, 6].Value);
+                                insObj.Email = Convert.ToString(workSheet.Cells[r, 7].Value);
+                                //insObj.SumAssured = Convert.ToDouble(workSheet.Cells[r, 8].Value);
+                                insObj.Premium = Convert.ToInt32(workSheet.Cells[r, 9].Value);
+                                insObj.Nominee = Convert.ToString(workSheet.Cells[r, 10].Value);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return "File uploaded";
+        }
+	}
+}
--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-﻿var appModule = angular.module('InsModule', []);//Module Created
+﻿var excelapp = angular.module('ExcelApp', []);
 
-appModule.controller('InsController', ['$scope',function($scope){
-    //console.log("Controller Invoked");
-    $scope.customer = {
-        Id: 0,
-        Name: '',
-        DOB:new Date()
-    }
+excelapp.controller('ExcelCtl', ['$scope', '$http',
+    function ($scope, $http) {
+      
+        $scope.ExcelFiles = [];
 
-    $scope.Save=function()
-    {
-        console.log($scope.customer.Id, '', $scope.customer.Name, '', $scope.customer.DOB);
+        var filetype = /application.vnd.openxmlformats-officedocument.spreadsheettml.sheet/;
 
-    }
 
-}]);
+        $scope.GetFiles = function (e) {
+
+            console.log("Event", e);
+            for(i=0;i<e.files.length;i++)
+            {
+                //if (e.files[i].type.match(filetype)) {
+                //    console.log("file type matching....");
+                    $scope.ExcelFiles.push(e.files[i]);
+               // }
+            }
+            console.log($scope.ExcelFiles);
+        }
+
+        $scope.UploadData=function()
+        {
+            var postFiles = new FormData();
+            for(let file in $scope.ExcelFiles)
+            {
+                postFiles.append("UploadedFile", $scope.ExcelFiles[file]);
+            }
+            $http.post('http://localhost:60543/ExcelUpload/ExcelFilePost', postFiles,
+                {
+                    withCredentials: false,
+                    headers: {
+                        'Content-Type':undefined
+                    },
+                    transformRequest: angular.identity
+                }).then(function(response){
+                    console.log(response);                
+                });
+        }
+
+
+    }]);
